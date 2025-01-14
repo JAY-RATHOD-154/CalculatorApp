2e66369e896d1457285c0a9f9364f975e2b667b7
JAY-RATHOD-154 <jayrathod154@gmail.com>
2025-01-14 14:38:15 +0530

    Initial commit

---
+++ app/asf.java
@@ -0,0 +1,145 @@
+public class asf {
+    import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+    public class MainActivity extends AppCompatActivity {
+
+        private EditText edtText;
+        private Button one, two, three, four, five, six, seven, eight, nine, zero;
+        private Button add, sub, mul, div, equals, clearText;
+
+        private double valueOne = Double.NaN;
+        private double valueTwo;
+
+        private char CURRENT_ACTION;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            // Initializing the EditText and Buttons
+            edtText = findViewById(R.id.edttext);
+            one = findViewById(R.id.one);
+            two = findViewById(R.id.two);
+            three = findViewById(R.id.three);
+            four = findViewById(R.id.four);
+            five = findViewById(R.id.five);
+            six = findViewById(R.id.six);
+            seven = findViewById(R.id.seven);
+            eight = findViewById(R.id.eight);
+            nine = findViewById(R.id.nine);
+            zero = findViewById(R.id.zero);
+            add = findViewById(R.id.add);
+            sub = findViewById(R.id.sub);
+            mul = findViewById(R.id.mul);
+            div = findViewById(R.id.div);
+            equals = findViewById(R.id.equals);
+            clearText = findViewById(R.id.clear_text);
+
+            // Setting up click listeners for numbers
+            View.OnClickListener listener = new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Button button = (Button) v;
+                    edtText.append(button.getText().toString());
+                }
+            };
+
+            one.setOnClickListener(listener);
+            two.setOnClickListener(listener);
+            three.setOnClickListener(listener);
+            four.setOnClickListener(listener);
+            five.setOnClickListener(listener);
+            six.setOnClickListener(listener);
+            seven.setOnClickListener(listener);
+            eight.setOnClickListener(listener);
+            nine.setOnClickListener(listener);
+            zero.setOnClickListener(listener);
+
+            // Setting up click listeners for operations
+            add.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    computeCalculation();
+                    CURRENT_ACTION = '+';
+                    edtText.setText(null);
+                }
+            });
+
+            sub.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    computeCalculation();
+                    CURRENT_ACTION = '-';
+                    edtText.setText(null);
+                }
+            });
+
+            mul.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    computeCalculation();
+                    CURRENT_ACTION = '*';
+                    edtText.setText(null);
+                }
+            });
+
+            div.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    computeCalculation();
+                    CURRENT_ACTION = '/';
+                    edtText.setText(null);
+                }
+            });
+
+            equals.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    computeCalculation();
+                    edtText.setText(String.valueOf(valueOne));
+                    valueOne = Double.NaN;
+                    CURRENT_ACTION = '0';
+                }
+            });
+
+            clearText.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    edtText.setText("");
+                    valueOne = Double.NaN;
+                    valueTwo = Double.NaN;
+                    CURRENT_ACTION = '0';
+                }
+            });
+        }
+
+        private void computeCalculation() {
+            if (!Double.isNaN(valueOne)) {
+                valueTwo = Double.parseDouble(edtText.getText().toString());
+                switch (CURRENT_ACTION) {
+                    case '+':
+                        valueOne += valueTwo;
+                        break;
+                    case '-':
+                        valueOne -= valueTwo;
+                        break;
+                    case '*':
+                        valueOne *= valueTwo;
+                        break;
+                    case '/':
+                        valueOne /= valueTwo;
+                        break;
+                }
+            } else {
+                valueOne = Double.parseDouble(edtText.getText().toString());
+            }
+        }
+    }
+
+}
